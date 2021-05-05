5ab5844d7b1278f53a996040dfa265b0303674e6
Ricky <rick.levan@gmail.com>
2021-05-05 17:08:41 -0500

    first

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+
+export default function Home() {
+  return (
+  <div style={{ color: `purple`}}>
+  	<Link to="/contact/">Contact</Link>
+  	<Header headerText="Landing Page"/>
+  	<p>What a World!</p>
+  	<img src="https://source.unsplash.com/random/400x200" alt="" />
+  	<button class="primary-button">Click me</button>
+  </div>)
+}
